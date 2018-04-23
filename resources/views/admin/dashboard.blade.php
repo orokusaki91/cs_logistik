d28f6edc132a301b4c5553cae4672539d344900b
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-04-23 16:26:19 +0200

    fix admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,6 +2,23 @@
 
 @section('title', 'Admin Dashboard')
 
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.19.0/sweetalert2.min.css">
+@stop
+
 @section('content')
-	Admin Panel
+	Admin Dashboard
+@stop
+
+@section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.19.0/sweetalert2.all.min.js"></script>
+	@if(Session::has('password_reseted'))
+		<script>
+			swal(
+		    'Erledigt!',
+		    '{{ Session::get('password_reseted') }}',
+		    'success'
+		    );
+		</script>
+	@endif
 @stop
---
+++ resources/views/partials/admin/_nav.blade.php
@@ -0,0 +1,9 @@
+    <header>
+    	<nav>
+    		<ul>
+    			<li>asdasd</li>
+    			<li>sdfdsf</li>
+    			<li>sdfdsfds</li>
+    		</ul>
+    	</nav>
+    </header>
