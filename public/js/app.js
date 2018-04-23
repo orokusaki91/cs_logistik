73496634d90ec9762e36028842de63e794539327
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-04-23 14:39:55 +0200

    fix app.js

--- public/js/app.js
+++ public/js/app.js
@@ -56,66 +56,63 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
-      // Menu-toggle button
 
-      $(document).ready(function() {
-        $(".menu-icon").on("click", function() {
-          $("nav ul").toggleClass("showing");
-        });
+// Menu-toggle button
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+    $("nav ul").toggleClass("showing");
+  });
+});
+
+// Scrolling Effect
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+    $('nav').addClass('black');
+  } else {
+    $('nav').removeClass('black');
+  }
+})
+
+$('.slider').each(function() {
+  
+  var $this = $(this);
+  var $group = $this.find('.slide-group');
+  var $slides = $this.find('.slide');
+  var buttonArray = [];
+  var currentIndex = 0;
+  var timeout;
+
+  function move(newIndex) {
+    var animateLeft, slideLeft;
+    
+    advance();
+    
+    if ($group.is(':animated') || currentIndex === newIndex) {
+      return;
+    }
+    
+    buttonArray[currentIndex].removeClass('active');
+    buttonArray[newIndex].addClass('active');
+    
+    if (newIndex > currentIndex) {
+      slideLeft = '100%';
+      animateLeft = '-100%';
+    } else {
+      slideLeft = '-100%';
+      animateLeft = '100%';
+    }
+    
+    $slides.eq(newIndex).css({left: slideLeft, display: 'block'});
+      $group.animate({left: animateLeft} , function() {
+        $slides.eq(currentIndex).css({display: 'none'});
+        $slides.eq(newIndex).css({left:0});
+        $group.css({left: 0});
+        currentIndex = newIndex;
       });
-
-      // Scrolling Effect
-
-      $(window).on("scroll", function() {
-        if($(window).scrollTop()) {
-          $('nav').addClass('black');
-        }
-
-        else {
-          $('nav').removeClass('black');
-        }
-      })
-
-      $('.slider').each(function() {
-        
-        var $this = $(this);
-        var $group = $this.find('.slide-group');
-        var $slides = $this.find('.slide');
-        var buttonArray = [];
-        var currentIndex = 0;
-        var timeout;
-
-        function move(newIndex) {
-          var animateLeft, slideLeft;
-          
-          advance();
-          
-          if ($group.is(':animated') || currentIndex === newIndex) {
-            return;
-          }
-          
-          buttonArray[currentIndex].removeClass('active');
-          buttonArray[newIndex].addClass('active');
-          
-          if (newIndex > currentIndex) {
-            slideLeft = '100%';
-            animateLeft = '-100%';
-          } else {
-            slideLeft = '-100%';
-            animateLeft = '100%';
-          }
-          
-          $slides.eq(newIndex).css({left: slideLeft, display: 'block'});
-          $group.animate({left: animateLeft} , function() {
-            $slides.eq(currentIndex).css({display: 'none'});
-            $slides.eq(newIndex).css({left:0});
-            $group.css({left: 0});
-            currentIndex = newIndex;
-          })
-        }
-        
-        function advance() {
-          clearTimeout(timeout);
+    }
+  
+  function advance() {
+    clearTimeout(timeout);
     //  Start timer to run anonymous function every # seconds.
     timeout = setTimeout(function() {
       if (currentIndex < ($slides.length - 1)) {
@@ -138,5 +135,4 @@ for (var i = 0; i < btns.length; i++) {
   });
   
   advance();
-  
 });
