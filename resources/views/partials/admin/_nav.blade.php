609ac00442223ec5df8d2c4798d60f8c25d699bd
dotlicweb <38036937+dotlicweb@users.noreply.github.com>
2018-04-24 09:39:37 +0200

    css

--- resources/views/partials/admin/_nav.blade.php
+++ resources/views/partials/admin/_nav.blade.php
@@ -1,6 +1,6 @@
 <nav>  
    <div class="admin_panel_head">
-      <h1>Administrator</h1>
+      <h1>CS Logistik dashboard</h1>
       <form action="{{ url('admin/logout') }}" method="post">
       	{{ csrf_field() }}
       	<button type="submit">Log out</button>
