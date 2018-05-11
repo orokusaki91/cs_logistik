fce64126fbf1b4c0b6f8802f3aa79af428ce2002
dotlicweb <38036937+dotlicweb@users.noreply.github.com>
2018-05-11 09:21:26 +0200

    css

--- resources/views/partials/admin/_nav.blade.php
+++ resources/views/partials/admin/_nav.blade.php
@@ -1,12 +1,14 @@
 <nav>  
-   <div class="admin_panel_head">
-    	<h1><a href="{{ url('admin') }}" style="text-decoration: none; color: #fff">CS Logistik Admin</a></h1>
-     	@if(Auth::check())
-			<form action="{{ url('admin/logout') }}" method="post">
-	      		{{ csrf_field() }}
-	      		<button type="submit">Log out</button>
+     @if(Auth::check())
+        <div class="admin_panel_head_inner">
+            <h1><a href="{{ url('admin') }}" style="text-decoration: none; color: #fff">CS Logistik Admin</a></h1>
+        
+            <form action="{{ url('admin/logout') }}" method="post">
+                {{ csrf_field() }}
+                <button type="submit">Log out</button>
                 <button type="submit" class="fa-power-off2"><i class="fa fa-power-off" aria-hidden="true"></i></button>
-	      	</form>
-     	@endif
-   </div>
+            </form>
+        </div>
+     @endif
 </nav>
+
